75641a469d2fbb05cc5de99888c3c1af1a788645
Rafsan <csrafsan700@gmail.com>
2024-07-15 06:11:54 +0600

    initial commit

---
+++ StudentPortel.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortel.Web.Models.Entities;
+
+namespace StudentPortel.Web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
